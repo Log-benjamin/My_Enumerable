301e9d3ac31c503f06fb13d0a10de47cb4919c4c
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-08-31 16:23:26 +0200

    Add my_list file and correct linters errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,26 +1,26 @@
 module MyEnumerable
-    # all? method
-    def all?
-      each do |e|
-        return false unless yield(e)
-      end
-      true
+  # all? method
+  def all?
+    each do |e|
+      return false unless yield(e)
     end
-  
-    # any? method
-    def any?
-      each do |e|
-        return true if yield(e)
-      end
-      false
-    end
-    
-    # filter method
-    def filter
-      result = []
-      each do |e|
-        result << e if yield(e)
-      end
-      result
-    end
+    true
   end
+
+  # any? method
+  def any?
+    each do |e|
+      return true if yield(e)
+    end
+    false
+  end
+
+  # filter method
+  def filter
+    result = []
+    each do |e|
+      result << e if yield(e)
+    end
+    result
+  end
+end
---
+++ my_list.rb
@@ -0,0 +1,28 @@
+require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
+
+  def initialize(*items)
+    @list = items
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+@list = MyList.new(1, 2, 3, 4, 5)
+
+# Test #all?
+# puts(list.all? {|e| e < 5})
+# => true
+# puts(list.all? {|e| e > 5})
+# => false
+
+# puts(list.any? {|e| e == 2})
+# => true
+# puts(list.any? {|e| e == 5})
+# => false
+
+# puts(list.filter {|e| e > 3})
+# => [2, 4]
